60bbc2f1a54636d8df98348c0898b8730b8107ad
GideonMax <45212668+GideonMax@users.noreply.github.com>
2020-06-04 16:12:33 +0300

    added exact copy

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ let app=express();
 app.get("/:name.js",(req,res)=>{
     res.sendFile(__dirname+`/${req.params.name}.js`);
 });
+app.get("/HereExample",(req,res)=>{
+    res.sendFile(__dirname+"/here.html");
+
+})
 app.get("/*",(req,res)=>{
     res.sendFile(__dirname+"/index.html");
 });
